095a45e61f068ff5cf99eb036d0a1c039d565b82
dimlaw7 <dimlaw7@gmail.com>
2023-04-23 12:41:25 +0100

    Update App.js
    Added useEffect to manage state for displaying page based on data from local storage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import Homepage from "./components/Homepage";
 import Splashscreen from "./components/Splashscreen";
 
 function App() {
-  const [ page, setPage ] = useState(false);
+  const [ page, setPage ] = useState(true);
+  useEffect(() => {
+    const getTodos = window.localStorage.getItem('initialTodos');
+    if (getTodos) {
+      const initialTodos = JSON.parse(getTodos);
+      if (initialTodos.length > 0) {
+        setPage(true);
+      }
+    }
+    else{
+      setPage(false);
+    }
+  }, [])
   return (
     <>
       { page ? <Homepage/> : <Splashscreen appState={setPage} /> }
