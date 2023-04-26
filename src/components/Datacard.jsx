fa27062d1768d2663959af5650e03afda1294b3d
dimlaw7 <dimlaw7@gmail.com>
2023-04-26 17:07:17 +0100

    Update Datacard.jsx
    Display percentage to show progress of the tasks.

--- src/components/Datacard.jsx
+++ src/components/Datacard.jsx
@@ -2,20 +2,22 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
-export const Datacard = ({ setModal }) => {
+export const Datacard = ({ setModal, todos }) => {
     const plusIcon = <FontAwesomeIcon icon={faPlus} />
+    const completedTodos = todos.filter((todo) => todo.status)
+    const progress = Math.floor(completedTodos.length / todos.length * 100) //Percentage progress
     return (
         <section className='data-card-sect'>
             <div className='data-card'>
                 <div className='flash-display'></div>
                 <h4 className='title'>Today's Project Summary</h4>
-                <span className='summary'>15 Tasks</span>
+                <span className='summary'>{todos.length} Tasks</span>
                 <div className='progress-bar'>
                     <span className='title'>Progress</span>
-                    <span className='summary'>14%</span>
-                    <progress id="file" value="32" max="100"> 32% </progress>
+                    <span className='summary'>{isNaN(progress) ? '0' : progress}% complete</span>
+                    <progress id="file" value={progress} max="100"> </progress>
                 </div>
-                <div className='add-task' onClick={() => setModal(true)}>{plusIcon}</div>
+                <div className='add-task' onClick={() => setModal({type: 'new-task', display: true,})}>{plusIcon}</div>
             </div>
         </section>
     )
