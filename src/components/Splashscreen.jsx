2028149a8c881de421120673cb76302ecaa4ec8d
dimlaw7 <dimlaw7@gmail.com>
2023-04-29 11:07:23 +0100

    Finish touches to design responsiveness

--- src/components/Splashscreen.jsx
+++ src/components/Splashscreen.jsx
@@ -1,8 +1,9 @@
 import task2 from './task2.svg';
+import './Splashscreen.css';
 
 const Splashscreen = ({ appState }) => {
     return (
-        <>
+        <div className='Splashscreen'>
             <section className='hero'>
                 <div className='img-container'>
                     <img src={task2} className='hero-img' alt='hero-img' />
@@ -13,7 +14,7 @@ const Splashscreen = ({ appState }) => {
                 <p>A platform that handles your tasks while you focus on other activities</p>
                 <button onClick={() => appState(true)}>Let's Start</button>      
             </section>
-        </>
+        </div>
     )
 }
 
